62c6a375696f8b1cffe769b5eadda5e7ee12cfae
Stine Richvoldsen <superrunt@gmail.com>
2014-05-06 08:59:10 -0600

    clean up

--- directives/toggle-class.js
+++ directives/toggle-class.js
@@ -2,11 +2,14 @@
  * @note Use: <div toggle-class="selector,class-to-toggle" />
  */
 angular.module('myApp').directive('toggleClass', function(){
-	return function(scope, element, attrs){
-		element.bind('click', function(){
-			var selector_class = attrs.toggleClass.split(',');
-			angular.element( document.querySelectorAll(selector_class[0]) )
-				.toggleClass(selector_class[1]);
-		});
-	};
+	return {
+            restrict: 'A',
+            link: function(scope, elem, attrs) {
+                element.bind('click', function(){
+                    var selector_class = attrs.toggleClass.split(',');
+                    angular.element( document.querySelectorAll(selector_class[0]) )
+                        .toggleClass(selector_class[1]);
+                });
+            }
+        };
 });
--- directives/toggle-sibling-class.js
+++ directives/toggle-sibling-class.js
@@ -2,11 +2,14 @@
  * @note Use: <div toggle-sibling-class="class-to-toggle" />
  */
 angular.module('myapp').directive('toggleSiblingClass', function(){
-	return function(scope, element, attr){
-		element.on('click', function(){
-			var _myself = angular.element(element);
-			var _siblings = _myself.parent().parent().children().children();
-			_siblings.toggleClass(attr.toggleSiblingClass);
-		});
-	};
+	return {
+            restrict: 'A',
+            link: function(scope, elem, attrs) {
+                element.bind('click', function(){
+                    var _myself = angular.element(element);
+					var _siblings = _myself.parent().parent().children().children();
+					_siblings.toggleClass(attr.toggleSiblingClass);
+                });
+            }
+        };
 });
